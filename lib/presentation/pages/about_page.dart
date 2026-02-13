8eff9c856404533ced0518d4b2254be31a9299f5
motebaya <davinsexo722@gmail.com>
2026-02-14 00:23:45 +0700

    chore(ui): bump about page version label to 2.1.0
    - update displayed semantic version
    
    - align UI version text with pubspec

--- lib/presentation/pages/about_page.dart
+++ lib/presentation/pages/about_page.dart
@@ -61,7 +61,7 @@ class AboutPage extends ConsumerWidget {
 
                           // Version
                           Text(
-                            'Version 2.0.0',
+                            'Version 2.1.0',
                             style: TextStyle(
                               fontSize: 14,
                               color: NeumorphismTheme.getSecondaryTextColor(isDark),
